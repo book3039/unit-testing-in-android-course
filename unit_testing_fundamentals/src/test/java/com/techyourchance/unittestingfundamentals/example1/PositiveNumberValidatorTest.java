e0eff28d00fb2cf4cbac003550be9deedf1c83c4
cih <book3039@knou.ac.kr>
2024-11-27 09:58:16 +0900

    Add test for NegativeNumberValidator in Exercise1

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/example1/PositiveNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/example1/PositiveNumberValidatorTest.java
@@ -18,18 +18,18 @@ public class PositiveNumberValidatorTest {
     @Test
     public void test1() {
         boolean result = SUT.isPositive(-1);
-        Assert.assertThat(result, is(false));
+        Assert.assertFalse(result);
     }
 
     @Test
     public void test2() {
         boolean result = SUT.isPositive(0);
-        Assert.assertThat(result, is(false));
+        Assert.assertFalse(result);
     }
 
     @Test
     public void test3() {
         boolean result = SUT.isPositive(1);
-        Assert.assertThat(result, is(true));
+        Assert.assertTrue(result);
     }
 }
