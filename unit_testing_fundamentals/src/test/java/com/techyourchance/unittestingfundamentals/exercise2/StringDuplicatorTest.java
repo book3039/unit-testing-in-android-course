6bdd738393fa2610805487fed7cdb314edbd7927
cih <book3039@knou.ac.kr>
2024-11-27 10:12:29 +0900

    Add test for StringDuplicator in Exercise2

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,35 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringDuplicatorTest {
 
+    StringDuplicator SUT;
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() {
+        String result = SUT.duplicate("");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void duplicate_singleCharacter_duplicatedCharactersReturned() {
+        String result = SUT.duplicate("a");
+        assertEquals("aa", result);
+    }
+
+    @Test
+    public void duplicate_longString_duplicatedStringReturned() {
+        String result = SUT.duplicate("book3039");
+        assertEquals("book3039book3039", result);
+    }
 }
